62aa62647df3d716f6cc9e320688e6b1e6dae385
Cadd47 <tcadd47@gmail.com>
2022-03-30 22:17:03 -0700

    beginning of building system

---
+++ Q4/Assets/Taylor/Scripts/BuildChecker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildChecker : MonoBehaviour
+{
+
+    BuildingManager BuildingManager;
+
+    public Collider[] centerCollider;
+
+    void Start()
+    {
+        BuildingManager = GameObject.Find("BuildingManager").GetComponent<BuildingManager>();
+    }
+
+    private void Update()
+    {
+        centerCollider = Physics.OverlapSphere(transform.position, 0.05f);
+        if (centerCollider.Length < 0)
+        {
+            Debug.Log("In");
+        }
+        else
+        {
+            Debug.Log("Out");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Building"))
+        {
+            BuildingManager.canPlace = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Building"))
+        {
+            BuildingManager.canPlace = true;
+        }
+    }
+}
---
+++ Q4/Assets/Taylor/Scripts/BuildingManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingManager : MonoBehaviour
+{
+    private Vector3 pos;
+
+    public Camera buildingCamera;
+
+    private RaycastHit hit;
+    [SerializeField] private LayerMask layerMask;
+
+    public GameObject[] objects;
+
+    public GameObject selectedObject;
+    public float gridSize;
+
+    public bool canPlace = true;
+
+    public float rotateAmount;
+
+    [SerializeField] private Material[] materials;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(selectedObject != null)
+        {
+            selectedObject.transform.position = new Vector3(RoundToNearestGrid(pos.x), RoundToNearestGrid(pos.y), RoundToNearestGrid(pos.z));
+
+            if (Input.GetMouseButtonDown(0) && canPlace)
+            {
+                PlaceObject();
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                RotateObject();
+            }
+
+            UpdateMaterials();
+        }
+    }
+
+    public void PlaceObject()
+    {
+        selectedObject.GetComponent<MeshRenderer>().material = materials[2];
+        selectedObject = null;
+    }
+
+    public void RotateObject()
+    {
+        selectedObject.transform.Rotate(Vector3.up, rotateAmount);
+    }
+
+    private void FixedUpdate()
+    {
+        Ray ray = buildingCamera.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            pos = hit.point;
+        }
+    }
+
+    void UpdateMaterials()
+    {
+        if (canPlace)
+        {
+            selectedObject.GetComponent<MeshRenderer>().material = materials[0];
+        }
+        else
+        {
+            selectedObject.GetComponent<MeshRenderer>().material = materials[1];
+        }
+    }
+
+    public void SelectObject(int index)
+    {
+        selectedObject = Instantiate(objects[index], pos, transform.rotation);
+    }
+
+    float RoundToNearestGrid(float pos)
+    {
+        float xDiff = pos % gridSize;
+        pos -= xDiff;
+        if(xDiff > (gridSize / 2))
+        {
+            pos += gridSize;
+        }
+        return pos;
+    }
+}
