9a4f79a3dd95d2c311fea54b3f61279ddcbd8809
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2022-04-01 09:50:14 -0700

    Added spend script

---
+++ Q4/Assets/Matve/Scriptos/SpendCurrency.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpendCurrency : MonoBehaviour
+{
+    public int woodCost;
+    public int stoneCost;
+    public int fishCost;
+    public int oreCost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spend()
+    {
+        ResourceManager.woodCount -= woodCost;
+        ResourceManager.stoneCount -= stoneCost;
+        ResourceManager.fishCount -= fishCost;
+        ResourceManager.oreCount -= oreCost;
+    }
+}
