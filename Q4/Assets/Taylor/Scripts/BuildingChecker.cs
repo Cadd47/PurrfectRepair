c9c027c7812717e4151411505fcfc81d57701943
Cadd47 <tcadd47@gmail.com>
2022-04-04 23:14:35 -0700

    the main building grid is actually semi complete now finally (I'm gonna cry, this stuffs is so difficult to understand pls help)

---
+++ Q4/Assets/Taylor/Scripts/BuildingChecker.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingChecker : MonoBehaviour
+{
+    BuildingManager buildingManager;
+
+    private int checkGood = 0;
+    private int checkBad = 0;
+
+    void Start()
+    {
+        buildingManager = GameObject.Find("BuildingManager").GetComponent<BuildingManager>();
+    }
+
+    void Update()
+    {
+        if (buildingManager.check == true)
+        {
+            PlaceCheck();
+        }
+
+        if (buildingManager.check == false)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void PlaceCheck()
+    {
+        Collider[] hitChecks = Physics.OverlapBox(transform.position, new Vector3(10, 10, 10));
+
+        foreach (Collider hitCollider in hitChecks)
+        {
+            if (hitCollider.gameObject.tag == "Building")
+            {
+                checkBad++;
+                checkGood = 0;
+            }
+            else
+            {
+                checkGood++;
+                checkBad = 0;
+
+            }
+        }
+
+        if (checkGood > checkBad)
+        {
+            //Debug.Log("Good");
+            buildingManager.canPlace = true;
+        }
+        else
+        {
+            //Debug.Log("Bad");
+            buildingManager.canPlace = false;
+        }
+    }
+}
--- Q4/Assets/Taylor/Scripts/BuildingManager.cs
+++ Q4/Assets/Taylor/Scripts/BuildingManager.cs
@@ -5,64 +5,61 @@ using UnityEngine.UI;
 
 public class BuildingManager : MonoBehaviour
 {
-    private Vector3 pos;
+    public GameObject[] structures;
+    public GameObject selectedObject;
+    public Material[] materials;
+    public bool check = true;
+    public bool canPlace = true;
 
-    public Camera buildingCamera;
+    public Camera buildCamera;
+    public float gridSize;
+
+    Vector3 pos;
+    public float rotateAmount;
 
     private RaycastHit hit;
     [SerializeField] private LayerMask layerMask;
 
-    public GameObject[] objects;
-
-    public GameObject selectedObject;
-    public float gridSize;
-
-    public bool canPlace = true;
-
-    public float rotateAmount;
-
-    [SerializeField] private Material[] materials;
-
-    // Update is called once per frame
     void Update()
     {
-
         if(selectedObject != null)
         {
-            selectedObject.transform.position = new Vector3(RoundToNearestGrid(pos.x), RoundToNearestGrid(pos.y), RoundToNearestGrid(pos.z));
+            selectedObject.GetComponent<Collider>();
+            selectedObject.transform.position = new Vector3(RoundToGrid(pos.x), 5, RoundToGrid(pos.z));
 
             if (Input.GetMouseButtonDown(0) && canPlace)
             {
                 PlaceObject();
             }
-            if (Input.GetKeyDown(KeyCode.R))
+
+            if(Input.GetKeyDown(KeyCode.R))
             {
                 RotateObject();
             }
+        }
 
-            UpdateMaterials();
+        UpdateMaterials();
+    }
+
+    private void FixedUpdate()
+    {
+        Ray ray = buildCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            pos = hit.point;
         }
     }
 
-    public void PlaceObject()
+    public void SelectObject(int index)
     {
-        selectedObject.GetComponent<MeshRenderer>().material = materials[2];
-        selectedObject = null;
+        check = true;
+        selectedObject = Instantiate(structures[index], pos, transform.rotation);
     }
 
     public void RotateObject()
     {
         selectedObject.transform.Rotate(Vector3.up, rotateAmount);
-    }
-
-    private void FixedUpdate()
-    {
-        Ray ray = buildingCamera.ScreenPointToRay(Input.mousePosition);
-
-        if(Physics.Raycast(ray, out hit, 1000, layerMask))
-        {
-            pos = hit.point;
-        }
     }
 
     void UpdateMaterials()
@@ -77,16 +74,21 @@ public class BuildingManager : MonoBehaviour
         }
     }
 
-    public void SelectObject(int index)
+    public void PlaceObject()
     {
-        selectedObject = Instantiate(objects[index], pos, transform.rotation);
+        selectedObject.GetComponent<MeshRenderer>().material = materials[2];
+
+        check = false;
+        selectedObject.GetComponent<Collider>().enabled = true;
+        selectedObject = null;
     }
 
-    float RoundToNearestGrid(float pos)
+    float RoundToGrid(float pos)
     {
         float xDiff = pos % gridSize;
         pos -= xDiff;
-        if(xDiff > (gridSize / 2))
+
+        if (xDiff > (gridSize / 2))
         {
             pos += gridSize;
         }
