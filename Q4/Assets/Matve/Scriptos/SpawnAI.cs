98b646c801757b0bf9b523cf7849fa95cb91505e
MatthewRacela <1707226@mukilteo.wednet.edu>
2022-04-11 20:07:30 -0700

    All done with AI spawning!

---
+++ Q4/Assets/Matve/Scriptos/SpawnAI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAI : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject woodHunt;
+    public GameObject stoneHunt;
+    public GameObject oreHunt;
+    public GameObject fishHunt;
+    public int wPrice;
+    public int sPrice;
+    public int oPrice;
+    public int fPrice;
+    //public GameObject witchHunt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnWood()
+    {
+        ResourceManager.fishCount -= wPrice;
+        Instantiate(woodHunt, spawnPoint);
+    }
+    public void spawnStone()
+    {
+        ResourceManager.fishCount -= sPrice;
+        Instantiate(stoneHunt, spawnPoint);
+    }
+    public void spawnOre()
+    {
+        ResourceManager.fishCount -= oPrice;
+        Instantiate(oreHunt, spawnPoint);
+    }
+    public void spawnFish()
+    {
+        ResourceManager.fishCount -= fPrice;
+        Instantiate(fishHunt, spawnPoint);
+    }
+
+}
