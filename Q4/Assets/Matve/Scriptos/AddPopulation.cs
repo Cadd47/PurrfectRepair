d1219c07e2786469a93148b223cc19ac20e814e8
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2022-04-14 10:13:44 -0700

    ACH

---
+++ Q4/Assets/Matve/Scriptos/AddPopulation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPopulation : MonoBehaviour
+{
+    public int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddPop()
+    {
+        Population.population += value;
+    }
+
+    public void RemovePop()
+    {
+        Population.population -= value;
+    }
+}
---
+++ Q4/Assets/Matve/Scriptos/Population.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Population : MonoBehaviour
+{
+    public static int population;
+    void Start()
+    {
+        population = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
