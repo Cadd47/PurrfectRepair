e3db91fe2750e882354e8abf061c29ff7c6b0f11
Cadd47 <tcadd47@gmail.com>
2022-04-26 06:16:39 -0700

    no message

---
+++ Q4/Assets/Taylor/Scripts/Options.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Options : MonoBehaviour
+{
+    public CinemachineFreeLook playerOneCam;
+    public CinemachineFreeLook playerTwoCam;
+
+    public float ySpeed = 9f;
+    public float xSpeed = 1500f;
+
+    private void AdjustY(float newY)
+    {
+        playerOneCam.m_YAxis.m_MaxSpeed = newY;
+        playerTwoCam.m_YAxis.m_MaxSpeed = newY;
+    }
+
+    private void AdjustX(float newX)
+    {
+        float multiplyX = newX * 1000;
+
+        playerOneCam.m_XAxis.m_MaxSpeed = multiplyX;
+        playerTwoCam.m_XAxis.m_MaxSpeed = multiplyX;
+    }
+}
