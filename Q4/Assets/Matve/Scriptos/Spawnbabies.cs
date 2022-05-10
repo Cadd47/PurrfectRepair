ea07ab2d21071d82e74acfa30f8f4a8239e6483e
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2022-05-10 10:14:49 -0700

    PUSHING

---
+++ Q4/Assets/Matve/Scriptos/Spawnbabies.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnbabies : MonoBehaviour
+{
+    public GameObject Cat;
+    public Transform spawn;
+    Collider col;
+    bool placed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = gameObject.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (col.enabled && !placed)
+        {
+            AddChild();
+            placed = true;
+        }
+
+    }
+
+    void AddChild()
+    {
+        Instantiate(Cat, spawn);
+    }
+
+}
